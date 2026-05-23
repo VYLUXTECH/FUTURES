acf47902d8448a157708aa0e542886498cc3b1a5
neonnoir83 <neonnoir83@gmail.com>
2026-05-23 15:01:41 +0000

    update SHA
    BY VYLUX AND RICHIE

--- scripts/wipe-and-setup.ps1
+++ scripts/wipe-and-setup.ps1
@@ -6,5 +6,5 @@ Start-Sleep -Seconds 2
 Remove-Item "$env:TEMP\setup.ps1" -Force -ErrorAction SilentlyContinue
 $wc = New-Object System.Net.WebClient
 $wc.Headers.Add("Cache-Control", "no-cache")
-$wc.DownloadFile("https://raw.githubusercontent.com/VYLUXTECHINC/FUTURES/1a964a7/scripts/setup-vps.ps1", "$env:TEMP\setup.ps1")
+$wc.DownloadFile("https://raw.githubusercontent.com/VYLUXTECHINC/FUTURES/6fd0d59/scripts/setup-vps.ps1", "$env:TEMP\setup.ps1")
 & "$env:TEMP\setup.ps1"
